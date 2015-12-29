5114020cf303d40476bbd29eabb0566c6d35c5e1
eggsy84 <eggsy@eggsylife.co.uk>
2015-12-29 15:14:05 +0000

    Renaming a few objects

---
+++ src/main/java/hello/CountPojo.java
@@ -0,0 +1,14 @@
+package hello;
+
+public class CountPojo {
+
+    private final long count;
+
+    public CountPojo(long count) {
+        this.count = count;
+    }
+
+    public long getCount() {
+        return count;
+    }
+}
